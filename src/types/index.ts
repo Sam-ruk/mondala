0c32bf8f5d84f2934f92e3fa5ffa4ba833735d23
Sam-ruk <samk.freelance@gmail.com>
2025-07-07 18:15:01 +0530

    Package Error solved

---
+++ src/types/index.ts
@@ -0,0 +1,36 @@
+import { MutableRefObject } from "react";
+
+export interface MousePosition {
+  x: number;
+  y: number;
+  lastX: number;
+  lastY: number;
+}
+
+export interface PathData {
+  x: number;
+  y: number;
+  ringIndex?: number; 
+  color?: string; 
+  penSize?: number; 
+}
+
+export interface CanvasConfig {
+  canvasRef: MutableRefObject<HTMLCanvasElement | null>;
+  ctxRef: MutableRefObject<CanvasRenderingContext2D | null>;
+  offscreenCanvasRef: MutableRefObject<HTMLCanvasElement | null>;
+  angleCountRef: MutableRefObject<number>;
+  colorRef: MutableRefObject<string>;
+  penSizeRef: MutableRefObject<number>;
+  activeRingRef: MutableRefObject<number>;
+  ringCountRef: MutableRefObject<number>;
+  pathDataRef: MutableRefObject<PathData[]>;
+  mousePos: MutableRefObject<MousePosition>;
+  isDrawing: MutableRefObject<boolean>;
+  needsDraw: MutableRefObject<boolean>;
+  isVibing: MutableRefObject<boolean>;
+  rotationRef: MutableRefObject<number>;
+  scaleRef: MutableRefObject<number>;
+  canvasSizeRef: MutableRefObject<number>;
+  analyserRef: MutableRefObject<AnalyserNode | null>;
+}
