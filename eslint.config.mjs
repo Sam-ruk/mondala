964feaf8ecfb91bfa85cce647a32860eddbe89a3
Sam-ruk <samk.freelance@gmail.com>
2025-07-07 18:44:16 +0530

    Fix ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,7 @@ export default [
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
       "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-explicit-any": "off"
     },
   },
 ];
