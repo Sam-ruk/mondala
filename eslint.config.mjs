1926837c82b10876fe8897e98bb3f54bb38cd694
Sam-ruk <samk.freelance@gmail.com>
2025-07-07 17:36:41 +0530

    Disable ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,8 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  extends: ['next'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off'
+    },
 ];
-
-export default eslintConfig;
